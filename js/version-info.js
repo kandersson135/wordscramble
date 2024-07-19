2478bd960590edac0cedd48c5438a5b6857b1963
kandersson135 <115895961+kandersson135@users.noreply.github.com>
2024-07-20 01:41:44 +0200

    heeeeee

--- js/version-info.js
+++ js/version-info.js
@@ -1,30 +1,4 @@
-/*
 $(document).ready(function() {
   var versionElement = $('#version-info p');
-  versionElement.text("Version: 0.0.9");
-});
-*/
-
-async function fetchManifest() {
-  try {
-    const response = await fetch('manifest.json');
-    const manifest = await response.json();
-    return manifest.version;
-  } catch (error) {
-    console.error('Error fetching manifest:', error);
-    return null;
-  }
-}
-
-async function displayVersion() {
-  const version = await fetchManifest();
-  if (version) {
-    $('#version-info p').text(`Version: ${version}`);
-  } else {
-    $('#version-info p').text('Version information not available');
-  }
-}
-
-$(document).ready(function() {
-    displayVersion();
+  versionElement.text("Version: 0.1.1");
 });
