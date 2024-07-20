2096966ff401522ae68dacacb7c2d9308095935b
kandersson135 <115895961+kandersson135@users.noreply.github.com>
2024-07-20 02:05:06 +0200

    Update version-info.js

--- js/version-info.js
+++ js/version-info.js
@@ -2,3 +2,24 @@ $(document).ready(function() {
   var versionElement = $('#version-info p');
   versionElement.text("Version: 0.1.1");
 });
+
+// Prevent long press to open up context menu
+document.addEventListener('contextmenu', function (e) {
+    e.preventDefault();
+}, false);
+
+document.addEventListener('touchstart', function (e) {
+    if (e.touches.length > 1) {
+        e.preventDefault();
+    }
+}, false);
+
+document.addEventListener('touchend', function (e) {
+    var now = new Date().getTime();
+    if ((now - lastTouchEnd) <= 300) {
+        e.preventDefault();
+    }
+    lastTouchEnd = now;
+}, false);
+
+var lastTouchEnd = 0;
